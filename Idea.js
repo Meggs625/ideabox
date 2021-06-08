89150cf0a776b7661c08be4439bf0cd11dcc4f20
Samantha Brown <bro7445@gmail.com>
2021-06-07 20:19:50 -0400

    Add save to storage method

--- Idea.js
+++ Idea.js
@@ -1,15 +1,22 @@
 class Idea {
-  constructor(id, title, body, star) {
-    this.id = id;
+  constructor(title, body) {
+    this.id = Date.now();
     this.title = title;
     this.body = body;
-    this.star = true || false;
+    this.star = false;
   }
+
   saveToStorage() {
-
+    var key = JSON.stringify(this.id);
+    var strungIdea = JSON.stringify(this);
+    localStorage.setItem(key, strungIdea);
+    // have to add something in here to put saved idea cards on page
   }
-  deleteFromStorage() {
-
+  deleteFromStorage(id) {
+    console.log("we here");
+  // for loop???
+    var key = JSON.stringify(this.id);
+    localStorage.remove(key);
   }
   updateIdea() {
 
